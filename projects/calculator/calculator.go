89370ba0e2356c52a2e058c071181e1a599d3dc2
Godbless Nyagawa <godblessnyagawa@gmail.com>
2025-01-05 09:09:20 +0300

    added golang calculator project code

--- projects/calculator/calculator.go
+++ projects/calculator/calculator.go
@@ -1,36 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// addition
-func addition(x, y float64)float64  {
+func addition(x, y float64) float64 {
 	return x + y
 }
 
-// subtraction
-func subtraction(x, y float64)float64  {
-	return x / y
+func subtraction(x, y float64) float64 {
+	return x - y
 }
 
-// multiplication
-func multiplication(x, y float64)float64  {
+func multiplication(x, y float64) float64 {
 	return x * y
 }
 
-// division
-func division(x, y float64)float64  {
-	if y != 0 {
-		return x / y
-	} else{
-		return 0
+func division(x, y float64) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("cannot divide by zero")
 	}
+
+	return x / y, nil
 }
-func main(){
+
+func help() {
+	fmt.Println("Welcome \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division")
+}
+
+func main() {
 	fmt.Println("WELCOME TO CALCULATOR APP.")
 
-	fmt.Println("\n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division")
+	fmt.Println("\n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. Help ")
 
-	var operation string
+	var operation int
 	fmt.Print("Enter operation number: ")
 	fmt.Scan(&operation)
+
+	// switch operation
+	switch operation {
+	case 1:
+		var number1 float64
+		var number2 float64
+
+		// allow user to enter first number and second
+		fmt.Print("Enter first number: ")
+		fmt.Scan(&number1)
+
+		fmt.Print("Enter second number: ")
+		fmt.Scan(&number2)
+
+		fmt.Println(addition(number1, number2))
+	case 2:
+		var number1 float64
+		var number2 float64
+
+		// allow user to enter first number and second
+		fmt.Print("Enter first number: ")
+		fmt.Scan(&number1)
+
+		fmt.Print("Enter second number: ")
+		fmt.Scan(&number2)
+
+		fmt.Println(subtraction(number1, number2))
+	case 3:
+		var number1 float64
+		var number2 float64
+
+		// allow user to enter first number and second
+		fmt.Print("Enter first number: ")
+		fmt.Scan(&number1)
+
+		fmt.Print("Enter second number: ")
+		fmt.Scan(&number2)
+
+		fmt.Println(multiplication(number1, number2))
+	case 4:
+		var number1 float64
+		var number2 float64
+
+		// allow user to enter first number and second
+		fmt.Print("Enter first number: ")
+		fmt.Scan(&number1)
+
+		fmt.Print("Enter second number: ")
+		fmt.Scan(&number2)
+
+		result, err := division(number1, number2)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(result)
+		}
+	case 5:
+		help()
+	default:
+		fmt.Println("Unknown operation!")
+	}
 }
